47000c1635d266a116804fd3f9939061639c7c96
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2025-03-18 07:42:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
 // Service Worker for PixelHeavenGames
-const CACHE_NAME = 'pixelheavengames-v1.0';
+const CACHE_NAME = 'pixelheavengames-v19.4';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/settings.css',
-  '/scripts.js',
-  '/settings.js',
-  '/manifest.json',
-  '/icon.png',
-  '/sounds/click.mp3',
-  '/Images/tic-tac-toe.jpg',
-  '/Images/snake.jpg'
+  './',
+  './index.html',
+  './styles.css',
+  './settings.css',
+  './scripts.js',
+  './settings.js',
+  './manifest.json',
+  './icon.png',
+  './sounds/click.mp3',
+  './Images/tic-tac-toe.jpg',
+  './Images/snake.jpg',
+  './Images/game3.jpg',
+  './Images/game4.jpg',
+  './Images/game5.jpg',
+  './Images/game6.jpg',
+  './Images/game7.jpg',
+  './Images/game8.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
 // Install event - cache assets
@@ -19,6 +26,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -52,4 +60,18 @@ self.addEventListener('activate', event => {
       );
     })
   );
+});
+
+// Listen for messages from clients
+self.addEventListener('message', event => {
+  if (event.data && event.data.action === 'updateCache') {
+    // Force update of cache
+    self.skipWaiting();
+    
+    // Re-cache all resources
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      });
+  }
 });
